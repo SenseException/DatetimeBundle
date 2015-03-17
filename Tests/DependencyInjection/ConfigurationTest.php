15faae0e82373d03730fc43c4596bc3edc6bde56
Claudio Zizza <claudio@budgegeria.de>
2015-03-18 00:54:17 +0100

    Configuration test created

---
+++ Tests/DependencyInjection/ConfigurationTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Budgegeria\Bundle\DatetimeBundle\Tests\DependencyInjection;
+
+use PHPUnit_Framework_TestCase;
+use Budgegeria\Bundle\DatetimeBundle\DependencyInjection\Configuration;
+
+/**
+ * @covers Configuration
+ */
+class ConfigurationTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers Configuration::getConfigTreeBuilder
+     * @uses Symfony\Component\Config\Definition\Builder\TreeBuilder
+     */
+    public function testGetConfigTreeBuilder()
+    {
+        $builder = (new Configuration())->getConfigTreeBuilder();
+
+        $tree = $builder->buildTree();
+
+        $this->assertInstanceOf('Symfony\Component\Config\Definition\ArrayNode', $tree);
+        $this->assertSame('budgegeria_datetime', $tree->getName());
+
+        $children = $tree->getChildren();
+        $this->assertCount(1, $children);
+
+        return $children;
+   }
+
+   /**
+    * @depends testGetConfigTreeBuilder
+    * @covers Configuration::getConfigTreeBuilder
+    * @uses Symfony\Component\Config\Definition\Builder\TreeBuilder
+    */
+   public function testTimezoneChild(array $children)
+   {
+       $this->assertArrayHasKey('timezone', $children);
+
+       /* @var $timezone \Symfony\Component\Config\Definition\ScalarNode */
+       $timezone = $children['timezone'];
+       $this->assertTrue($timezone->isRequired());
+       $this->assertSame('values like UTC, America/Los_Angeles, Europe/Paris', $timezone->getExample());
+   }
+}
